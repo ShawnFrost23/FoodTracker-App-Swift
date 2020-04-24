b72ccfc4129692628a843428485e0b7752eccc0e
Arth Patel <arthpatelf50@gmail.com>
2020-04-24 19:23:59 +1000

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Arth Patel on 24/4/20.
+//  Copyright © 2020 Arth Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
